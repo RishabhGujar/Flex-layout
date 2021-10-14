b3e5919468c059b0fc3d8bb7c7c3c789a787005a
RishabhGujar <rishabhgurjar101@gmail.com>
2021-10-14 17:36:07 +0530

    "updates-on-styles"

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const clock = document.querySelector("#digitalClock");
 
 function getTime(){
   let time = new Date().toTimeString();
-  clock.textContent =time.slice(0,8) + (time.slice(0,3)>12 ? " pm" : " am");
+  clock.textContent =time.slice(0,8) + (time.slice(0,2)>=12 ? " pm" : " am");
+  console.log(time.slice(0,2));
 }
 
 setInterval(getTime,1000);
